601b826d0f10f86bd73a123bafaf47f77965e18c
Nafi-GOAT <nafih2989@gmail.com>
2025-11-14 21:05:17 -0800

    3

--- main.cpp
+++ main.cpp
@@ -5,14 +5,34 @@
 using namespace std;    
 
 int main () {
-    const int NUM_CARS = 2;
+    const int INITIAL_CARS = 2;
     const int PAY_PROBABILITY = 55;
 
     srand(time(0));
 
     deque<Car> lane;
 
-    for (int i = 1; i < NUM_CARS; ++i){
+    for (int i = 0; i < INITIAL_CARS; ++i){
         lane.push_back(Car());
     }
+
+    cout << "Initial queue:\n";
+    if (lane.empty()) {
+        cout << "Empty\n";
+    } else {
+        for (Car &c : lane) {
+            cout << "    ";
+            c.print();
+            cout << endl;
+        }
+    }
+    cout << endl;
+
+    int time = 0;
+
+     
+    while (!lane.empty()) {
+    +++ timeStep = 1;
+
+    int roll = rand() % 100;
 }
